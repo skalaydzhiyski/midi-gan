b435a336798cb2e5095759f4a249425373ca712c
Spas Kalaydzhisyki <spas.kalaydzhiyski@gmail.com>
2021-01-16 13:02:33 +0000

    first push now seriously :)

---
+++ conf.py
@@ -0,0 +1,10 @@
+
+class Path: 
+  MP3_DOWNLOAD_PATH   = './data/mp3/'
+  INPUT_DATA_PATH     = './data/input/'
+  OUTPUT_DATA_PATH    = './data/output/'
+  TRAIN_DATA_PATH     = './data/train/'
+  METADATA_PATH       = './data/metadata/'
+  SPLEETER_PATH       = './data/spleeter/'
+
+
