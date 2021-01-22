b07b6b56d0d6fed12e1f9214e2c21606bd1e049d
Spas Kalaydzhisyki <spas.kalaydzhiyski@gmail.com>
2021-01-22 18:03:07 +0000

    finished the generator model

--- const.py
+++ const.py
@@ -1,6 +1,6 @@
 
 class Const:
   # number of rows from the converted track to include in a single chunk
-  TRACK_PART_SIZE = 4098
+  TRACK_PART_SIZE = 4096    # nice and beautiful power of 2
   N_PIANO_NOTES = 88
 
--- model.py
+++ model.py
@@ -4,15 +4,20 @@ import torch.nn.functional as F
 
 
 class Generator(nn.Module):
-  def __init__(self, noise):
-    self.noise = noise
-    # Tconv1
-    tconv1 = nn.ConvTranspose1d(in_channels=1, out_channels=1, kernel_size=129, stride=1, padding=0, bias=False)
-    # Tconv2
-    tconv2 = nn.ConvTranspose1d(in_channels=1, out_channels=1, kernel_size=257, stride=1, padding=0, bias=False)
-    # Tconv3
-    tconv3 = nn.ConvTranspose1d(in_channels=1, out_channels=1, kernel_size=513, stride=1, padding=0, bias=False)
-    # Tconv4 to 2D for the piano roll matrix 
-    # TODO: Figure this out when the HHKB comes here (and whern there's no more work in Santander)
+  def __init__(self, noise_dim=128):
+    super().__init__()
+    self.noise = torch.randn(1,1,noise_dim)
+    self.tconv1 = nn.ConvTranspose1d(in_channels=1, out_channels=1, kernel_size=129, stride=1, padding=0, bias=False)
+    self.tconv2 = nn.ConvTranspose1d(in_channels=1, out_channels=1, kernel_size=257, stride=1, padding=0, bias=False)
+    self.tconv3 = nn.ConvTranspose1d(in_channels=1, out_channels=1, kernel_size=513, stride=1, padding=0, bias=False)
+    self.tconv4 = torch.nn.ConvTranspose2d(1024, 1, (88,4096), 1, 0, bias=False)
+
+  def forward(self):
+    res = self.tconv1(self.noise)
+    res = self.tconv2(res)
+    res = self.tconv3(res)
+    res = res.reshape(1,1024,1,1)
+    res = self.tconv4(res)
+    return res
 
   
