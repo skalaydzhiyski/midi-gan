5100a82c617b987a2f52deafec55fc57c31e83aa
Spas Kalaydzhisyki <spas.kalaydzhiyski@gmail.com>
2021-01-23 17:05:05 +0000

    all ready for writing the training loop

---
+++ dataset.py
@@ -0,0 +1,17 @@
+
+class Dataset:
+  def __init__(self, x, transform):
+    self.x = x
+    self.transform = transform
+
+  def __getitem__(self, idx):
+    if self.transform is not None:
+      return self.transform(self.x[idx])
+    return self.x[idx]
+
+  def __len__(self):
+    return len(self.x)
+
+  def __repr__(self):
+    return f'Dataset [shape: {self.x.shape}]'
+
--- run_trainer.py
+++ run_trainer.py
@@ -1,11 +1,14 @@
 #!./bin/python
 import torch
+from torchvision import transforms
+import numpy as np
+
 import random
- 
+
 from model import Generator, Discriminator
 from const import Const
 from conf import Path
-from data import Dataset
+from dataset import Dataset
 
 
 # params
@@ -13,30 +16,49 @@ dataroot   = Path.TRAIN_DATA_PATH
 workers    = 2
 batch_size = 2    # todo: increase
 n_epochs   = 1    # todo: increase
-lr         = 0.2
+lr         = .02
 beta1      = .5
 ngpu       = 0    # todo: set to the number of gpus in the training machine.
 device     = torch.device('cuda:0' if (torch.cuda.is_available() and ngpu > 0) else 'cpu') 
 
 # we need reporoducible results
-manual_seed = 1948
-manual_seed = random.randint(1, 10000)
-print(f'Random seed: {seed}')
-random.seed(manual_seed)
+seed = 1948
+#seed = random.randint(1, 10000)
+random.seed(seed)
+torch.manual_seed(seed)
 
+
+def stats(x):
+  return x.mean(), x.std()
+
+def test_loader(train_dl):
+  batch = next(iter(train_dl))
+  print(batch.shape)
 
 def run_training_loop():
-  noise = torch.randn(batch_size, 1, 128)
-  #gen = Generator(noise, ngpu)
-  #print(gen)
-  
-  # get train.npy into a dataset format 
-
-  # noirmalize the dataset
+  # build a dataset instance.
+  transform = transforms.Compose([
+      transforms.ToTensor(),
+      transforms.Normalize((.5,.5,.5),(.5,.5,.5)) 
+  ])
+  train_data = np.load('./data/train/train.npy')
+  print(stats(torch.tensor(train_data).float()))
+  train_ds = Dataset(train_data, transform=transform)
+  print(f'Training data: {train_ds}')
 
   # create a data loader
+  train_dl = torch.utils.data.DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=workers)
+  test_loader(train_dl)
 
-  # write the training loop here:
+  # init models 
+  noise = torch.randn(2, 1, 128)
+  gen = Generator(noise, ngpu)
+  dis = Discriminator(ngpu)
+  print(gen, dis)
+  
+  for i, epoch in enumerate(range(n_epochs)):
+    for i, real_batch in enumerate(train_dl):
+      # TODO: write the training loop here 
 
   # Generate samples
 
